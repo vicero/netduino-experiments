3ebbe19601d89a4344ff84e39ecf70796d921502
vicero <james.kessler@gmail.com>
2015-05-10 17:04:44 -0600

    Organize projects and solution

---
+++ Dht11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using CW.NETMF;
+using CW.NETMF.Sensors;
+using Microsoft.SPOT;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Dht11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // TODO: Change pins and pull-up resistor type according to the actual wiring
+            var dhtSensor = new Dht11Sensor(Pins.GPIO_PIN_D0, Pins.GPIO_PIN_D1, PullUpResistor.Internal);
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+
+                if (dhtSensor.Read())
+                {
+                    Debug.Print("DHT sensor Read() ok, RH = " + dhtSensor.Humidity.ToString("F1") + "%, Temp = " + dhtSensor.Temperature.ToString("F1") + "°C");
+                }
+                else
+                {
+                    Debug.Print("DHT sensor Read() failed");
+                }
+            }
+        }
+
+    }
+}
